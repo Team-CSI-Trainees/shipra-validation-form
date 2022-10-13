4e4d065d478bb97a939414c816dcc6982fde1574
Shipra <shipratripathi.500@gmail.com>
2022-10-13 21:45:28 +0530

    update

--- script.js
+++ script.js
@@ -11,11 +11,16 @@ const cpassword = document.getElementById('cpassword');
     event.preventDefault();
     validate();
   })
+
+  // var userCheck = /^[A-Za-z. ]{3,25}$/;
+  // var passwordCheck = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,18}$/;
+  // var emailCheck = /^[a-zA-Z0-9_.]{3,}[@][a-zA-Z]{3,}[.]{1}[A-Za-z.]{2,8}$/;
+  // var phoneCheck = /^[6-9]{10}$/;
   
- 
-
-   var usernameVal  = /^[A-Za-z. ]{3,25}$/;
-
+  
+   
+  
+  
 
 
   //define the validate function
@@ -26,7 +31,8 @@ const phoneVal = phone.value.trim();
 const passwordVal = password.value.trim();
 const cpasswordVal= cpassword.value.trim();
 // const genderVal =  gender.value.trim();  
-
+   
+   
   //username
   if(usernameVal === "" ){
    setErrorMsg(username, 'username cannot be blank');
@@ -40,8 +46,9 @@ const cpasswordVal= cpassword.value.trim();
     //email
    if(emailVal === "" ) {
    setErrorMsg(email, 'email cannot be blank');
-   } 
-   
+  //  } else if ( !isEmail(emailVal)){
+  //   setErrorMsg(emailVal, 'Not a valid Email');
+   }
    else{
     setSuccessMsg(email);
    }
@@ -77,13 +84,14 @@ const cpasswordVal= cpassword.value.trim();
   }
 
   //function 
-  function setErrorMsg( input , Errormsg){
+  function setErrorMsg( input , errormsgs){
     const formControl = input.parentElement;
     const small = formControl.querySelector('small');
     formControl.className="form-control error";
-    small.innerHTML = Errormsg;
+    small.innerHTML = errormsgs;
  }
   function setSuccessMsg(input) {
     const formControl= input.parentElement;
     formControl.className="form-control success";
   }
+
